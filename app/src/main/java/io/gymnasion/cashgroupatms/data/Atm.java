3b2625804724f25ec4c2a8b51acb0291e6e2f15a
xella <xella.design@gmail.com>
2017-12-12 23:54:54 +0100

    Updated atm model

--- app/src/main/java/io/gymnasion/cashgroupatms/data/Atm.java
+++ app/src/main/java/io/gymnasion/cashgroupatms/data/Atm.java
@@ -5,7 +5,7 @@ public class Atm {
     private String name, city, street, zip;
     private double lat, lng;
 
-    public Atm(String name, String city, String street, String zip, double lat, double lng ) {
+    public Atm(String name, String city, String street, String zip, double lat, double lng) {
         this.name = name;
         this.city = city;
         this.street = street;
